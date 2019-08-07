8c83d3252e1203e2481a8924c997a81c74c04881
Romil <romilbalar.cs17@bmsce.ac.in>
2019-08-07 12:01:30 +0530

    LAB

---
+++ lab0.py
@@ -0,0 +1,10 @@
+num_array=list()
+def max():
+    N=int(input("Enter N"))
+    max=0
+    for i in range(0,N):
+        A[i]=input("Enter number")
+        if A[i]>max:
+            max=A[i]
+
+    print("Maximum number",max)
