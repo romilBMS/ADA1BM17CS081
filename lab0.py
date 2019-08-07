0ac8e365d02e3562c245f4771de619e754790990
Romil <romilbalar.cs17@bmsce.ac.in>
2019-08-07 12:16:51 +0530

    final

--- lab0.py
+++ lab0.py
@@ -1,10 +1,12 @@
-num_array=list()
-def max():
+def getmax():
+    A=list()
     N=int(input("Enter N"))
     max=0
     for i in range(0,N):
-        A[i]=input("Enter number")
+        A.append(int(input("Enter number")))
         if A[i]>max:
             max=A[i]
 
-    print("Maximum num",max)
+    print("Maximum number",max)
+    
+getmax()
