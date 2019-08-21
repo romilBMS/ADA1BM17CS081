60018181db032d0182a76e0ec2509d461b908e3f
Romil <romilbalar.cs17@bmsce.ac.in>
2019-08-21 11:49:52 +0530

    lab2b

---
+++ lab2b.py
@@ -0,0 +1,12 @@
+def findkl(A,k):
+    n=len(A)
+
+    for i in range(k):
+        for j in range(n-1-i):
+            if A[j]>A[j+1]:
+                A[j],A[j+1]=A[j+1],A[j]
+            
+    return A[n-k:]
+
+A=[2,1,3,6,5,4]
+print(findkl(A,1))
